61f83af4a5e12396562cc5c64d7398bfb656a7b4
Mohamed Elkarmi <melkarmi@e2r6p4.1337.ma>
2021-10-23 16:54:54 +0100

    ex02 added to module01

---
+++ module01/ex02/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melkarmi <melkarmi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/23 16:26:22 by melkarmi          #+#    #+#             */
+/*   Updated: 2021/10/23 16:53:37 by melkarmi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main ()
+{
+    std::cout << "program is running "<< std::endl;
+    std::string s("HI THIS IS BRAIN");
+    std::string *stringPTR = &s;
+    std::string &stringREF = s;
+
+
+    std::cout << "Display the address::" <<  std::endl;
+    std::cout << "      address of the string  : "<< &s <<  std::endl;
+    std::cout << "      address by the pointer : "<< stringPTR <<  std::endl;
+    std::cout << "      address by the ref     : "<< &stringREF <<  std::endl;
+     
+    std::cout << "Display the string::" <<  std::endl;
+    std::cout << "      string  by the pointer : "<< *stringPTR <<  std::endl;
+    std::cout << "      string  by the ref     : "<< stringREF <<  std::endl;
+
+    std::cout << "program exitng ! " <<  std::endl;
+    
+}
