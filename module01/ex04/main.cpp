037b7d34c76e58f3ffd32e03114df00097de9b21
mohamed Elkarmi <yaiba@mohameds-MacBook-Pro.local>
2021-10-25 12:10:44 +0100

    quick update on ex04  module01

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: melkarmi <melkarmi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yaiba <yaiba@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 16:18:41 by melkarmi          #+#    #+#             */
-/*   Updated: 2021/10/24 16:43:59 by melkarmi         ###   ########.fr       */
+/*   Updated: 2021/10/25 11:14:17 by yaiba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,50 @@ bool  handle_errors(int ac, char *av[])
 
 int main (int ac, char *av[])
 {
+    std::string line;
+    std::string ret;
+    int start;
+    std::string str;
+    std::string s1 = "ss";
+    std::string s2 = "kk";
+    size_t indx;
+
     if (handle_errors(ac , av) == true)
         return (1);
     std::ifstream fs(av[1]);
-    strcat(av[1], ".replace");
-    std::ofstream MyFile(av[1]);
+    std::string newfile_name = av[1];
+    newfile_name = newfile_name + ".replace";
+    std::ofstream MyFile(newfile_name);
+    s1 = av[2];
+    s2 = av[3];
+    while (std::getline(fs, line))  
+    {
+
+    
+        
+        str = "";
+        start = 0;
+        size_t indx = line.find(s1, start);
+        if (indx >= std::string::npos)
+            str = line;  
+        else
+        {    
+            while(indx != std::string::npos)
+            {
+                ret = line.substr(start,  indx - start);
+                str = str + ret;
+                start = indx + s2.size();
+                str.insert(indx, s2);
+                indx = line.find(s1,indx+1);
+
+            }
+            ret = line.substr(start,  line.size());
+            str = str + ret;     
+
+        }   
+        str = str + "\n";
+        MyFile << str;
+        
+    }
+    MyFile.close();
 }
